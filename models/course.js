fe2fae9d60eee24fde331da7af858248a9e72e88
LeeVaughn <dlvprogramming@gmail.com>
2020-02-19 09:02:30 -0800

    created Course model

---
+++ models/course.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize) => {
+  class Course extends Sequelize.Model {}
+
+  Course.init({
+    title: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Title is required"
+        }
+      }
+    },
+    description: {
+      type: Sequelize.TEXT,
+      validate: {
+        notEmpty: {
+          msg: "Description is required"
+        }
+      }
+    },
+    estimatedTime: Sequelize.STRING,
+    materialsNeeded: Sequelize.STRING
+  }, { sequelize });
+
+  return Course;
+}
