5a5fca1bd6e566b172ee0b9b870abb85aff3940f
LeeVaughn <dlvprogramming@gmail.com>
2020-02-20 22:08:43 -0800

    created initial file and mapped out future routes

---
+++ routes/courses.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const Course = require("../models").Course;
+const User = require("../models").User;
+const Sequelize = require('sequelize');
+
+// handler function to wrap each route
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+}
+
+// returns a list of all courses
+router.get("/", asyncHandler(async (req, res) => {
+
+}));
+
+// returns a specific course based on the provided course id
+router.get("/:id", asyncHandler(async (req, res) => {
+
+}));
+
+// creates a new course
+router.post("/", asyncHandler(async (req, res) => {
+
+}));
+
+// updates a specific course based on the provided course id
+router.put("/", asyncHandler(async (req, res) => {
+
+}));
+
+// deletes a specific course based on the provided course id
+router.delete("/", asyncHandler(async (req, res) => {
+
+}));
---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models").User;
+const Course = require("../models").Course;
+const Sequelize = require('sequelize');
+
+// handler function to wrap each route
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+}
+
+// returns the currently authenticated user
+router.get("/", asyncHandler(async (req, res) => {
+
+}));
+
+// creates a new user
+router.post("/", asyncHandler(async (req, res) => {
+
+}));
