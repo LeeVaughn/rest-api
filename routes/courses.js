ffa17ed7050273ec3229d451712c25671b084f78
LeeVaughn <dlvprogramming@gmail.com>
2020-02-23 13:08:04 -0800

    finished /courses and .courses/:id GET routes

--- routes/courses.js
+++ routes/courses.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Course = require("../models").Course;
 const User = require("../models").User;
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
 // handler function to wrap each route
 function asyncHandler(cb) {
@@ -15,14 +15,28 @@ function asyncHandler(cb) {
   }
 }
 
-// returns a list of all courses
+// returns a list of all courses and the user that they belong to
 router.get("/", asyncHandler(async (req, res) => {
-  console.log("working");
+  const courses = await Course.findAll({
+    include: [{ model: User, attributes: { exclude: ["password", "createdAt", "updatedAt"] } }],
+    attributes: { exclude: ["createdAt", "updatedAt"] }
+  });
+
+  res.json(courses);
 }));
 
-// returns a specific course based on the provided course id
+// returns a specific course and the user it belongs to based on the provided course id
 router.get("/:id", asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id, {
+    include: [{ model: User, attributes: { exclude: ["password", "createdAt", "updatedAt"] } }],
+    attributes: { exclude: ["createdAt", "updatedAt"] }
+  });
 
+  if (course) {
+    res.json(course);
+  } else {
+    res.status(404).json({ "message": "Course not found" });
+  }
 }));
 
 // creates a new course
