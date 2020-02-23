64c9f9bc0b35ddbad9517a63c5030909de9c21f1
LeeVaughn <dlvprogramming@gmail.com>
2020-02-22 17:59:00 -0800

    working on /users GET and POST routes

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ function asyncHandler(cb) {
     try {
       await cb(req, res, next)
     } catch (error) {
+      console.log(error)
       res.status(500).send(error);
     }
   }
@@ -17,12 +18,17 @@ function asyncHandler(cb) {
 
 // returns the currently authenticated user
 router.get("/", asyncHandler(async (req, res) => {
-  console.log("working");
+  console.log("users");
+  const users = await User.findAll();
+  res.json(users);
 }));
 
 // creates a new user
 router.post("/", asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const user = await User.create(req.body);
 
+  res.status(201).end();
 }));
 
 module.exports = router;
