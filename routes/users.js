e15548a4adc55d38409320e0e54d57c1b366f837
LeeVaughn <dlvprogramming@gmail.com>
2020-02-22 21:16:51 -0800

    completed /users POST route

--- routes/users.js
+++ routes/users.js
@@ -18,17 +18,33 @@ function asyncHandler(cb) {
 
 // returns the currently authenticated user
 router.get("/", asyncHandler(async (req, res) => {
-  console.log("users");
   const users = await User.findAll();
   res.json(users);
 }));
 
 // creates a new user
 router.post("/", asyncHandler(async (req, res) => {
-  console.log(req.body);
-  const user = await User.create(req.body);
+  let user
+  try {
+    user = await User.create(req.body);
 
-  res.status(201).end();
+    res.status(201).location("/").end();
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      // iterates over error to create an array of error messages
+      const errors = error.errors.map(err => err.message);
+
+      res.status(400).json(errors);
+    } else if (error.name === "SequelizeUniqueConstraintError") {
+      // iterates over error to create an array of error messages
+      const errors = error.errors.map(err => err.message);
+
+      res.status(400).json(errors);
+    } else {
+      // handled by asyncHandler's catch block
+      throw error;
+    }
+  }
 }));
 
 module.exports = router;
